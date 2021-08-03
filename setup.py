175f81c7d4e8091c94e0231acb71421ec975b46a
Nuno Fachada <nuno.fachada@ulusofona.pt>
2021-08-03 19:46:34 +0100

    Improve argument parsing in script for running simulations

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     packages = ['uavnoma'],
     install_requires = ['numpy', 'matplotlib'],
     python_requires = '>=3.8',
-    scripts=['bin/plot_figures'],
+    scripts=['bin/uavnoma-cli'],
     include_package_data = True,
 
 )
