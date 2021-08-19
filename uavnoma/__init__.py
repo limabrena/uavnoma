88fd1fb45e5500e7035c0199b0e6aa0cba548a68
Brena Lima <brenakslima@gmail.com>
2021-08-19 16:48:10 +0100

    Update uavnoma tutorial

--- uavnoma/__init__.py
+++ uavnoma/__init__.py
@@ -10,6 +10,7 @@ from .generate_values import fading_rician
 from .generate_values import random_position_uav
 from .generate_values import random_position_users
 from .generate_values import generate_channel
+from .parameters_validation import validation
 from .performance_metrics import calculate_instantaneous_rate_primary
 from .performance_metrics import calculate_instantaneous_rate_secondary
 from .performance_metrics import average_rate
