842727c5502c6b02c852ee9e3938e42667f8cdb5
Brena Lima <brenakslima@gmail.com>
2021-08-26 00:18:16 +0100

    Update test rate primary user

--- tests/test_performance_metrics.py
+++ tests/test_performance_metrics.py
@@ -1,13 +1,13 @@
 import numpy as np
-import math
 from uavnoma.performance_metrics import *
-from uavnoma.generate_values import fading_rician, generate_channel
+from uavnoma.generate_values import generate_channel
 import pytest
 
 # Variable simulation parameters
 monte_carlo_samples = 1000  # Monte Carlo samples
 snr_dB = np.array(range(10, 51, 2))  # SNR in dB
 snr_linear = 10 ** (snr_dB / 10)  # SNR linear
+
 # Initialization of some auxiliary arrays
 out_probability_system = np.zeros((monte_carlo_samples, len(snr_dB)))
 out_probability_secondary_user = np.zeros((monte_carlo_samples, len(snr_dB)))
@@ -15,48 +15,60 @@ out_probability_primary_user = np.zeros((monte_carlo_samples, len(snr_dB)))
 system_average_rate = np.zeros((monte_carlo_samples, len(snr_dB)))
 rate_secondary_user = np.zeros((monte_carlo_samples, len(snr_dB)))
 rate_primary_user = np.zeros((monte_carlo_samples, len(snr_dB)))
-target_rate_primary_user = 0.5 # Target rate bits/s/Hertz  primary user
-target_rate_secondary_user = 0.5 # Target rate bits/s/Hertz  secondary user
 
+#
+# Valid and invalid parameters for testing the performance_metrics module
+#
 
-data_parameter_rate = [    
-    ( # Valid
-        [2,    # number_user
-        10.0,  # axis x user position
-        6.0,   # axis y user position
+data_parameter_rate_valid = [    
+    ( 
+        2,    # number of users
+        np.array([10.0, 6.0]),  # array axis x user 1 and user 2 position
+        np.array([2.0, 8.0]),   # array axis y user 1 and user 2 position
         2.0,   # axis x uav position
         4.0,   # axis y uav position
         35.0,  # uav mean height
-        2.0,   # path_loss,
-        15.0,  # rician_factor
-        1.0,   # power_los
+        2.0,   # path loss exponent,
+        0.03,  # mean rician fading
+        1.3,   # standard deviation
         0.8,   # power_primary
         0.2,   # power_secondary 
         0.05,  # hardware impairments
-        0.05]
-        ), # imperfect SIC coeffcient
-    ( # Invalid
-        [4,    # number_user
-        25.0,  # axis x user position
-        6.0,   # axis y user position
+        0.05,  # imperfect SIC coeffcient
+    ) 
+]
+data_parameter_rate_invalid = [    
+    ( 
+        4,    # number of users
+        np.array([7.0, -16.0]),   # array axis x user 1 and user 2 position
+        np.array([25.0, 10.0]),   # array axis y user 1 and user 2 position
         6.0,   # axis x uav position
         4.0,   # axis y uav position
         80.0,  # uav mean height
-        2.0,   # path_loss,
-        2.0,  # rician_factor
-        3.0,   # power_los
-        0,   # power_primary
+        2.0,   # path loss exponent,
+        -0.1,  # mean rician fading
+        3.0,   # standard deviation
+        0.0,   # power_primary
         0.9,   # power_secondary 
         0.05,  # hardware impairments
-        0.05]
-        ), # imperfect SIC coeffcient
+        1.1,   # imperfect SIC coeffcient
+    ) 
 ]
 
-# Test generate channel
-@pytest.mark.parametrize("number_user, x_u, y_u, x_r, y_r, uav_height_mean, path_loss, rician_factor, power_los, power_primary, power_secondary, hardw_ip, sic_ip", data_parameter_rate)
-def test_rate_primary_user(number_user, x_u, y_u, x_r, y_r, uav_height_mean, path_loss, rician_factor, power_los, power_primary, power_secondary, hardw_ip, sic_ip):
+data_target_rate_valid = [
+    # Target rate primary user, Target rate secondary user
+    ([0.5, 0.5])
+]
+
+data_target_rate_invalid = [
+    # Target rate primary user, Target rate secondary user
+    ([2, -4])
+]
+
+# Test rate primary user (valid parameters)
+@pytest.mark.parametrize("number_user, x_u, y_u, x_r, y_r, uav_height_mean, path_loss, s, sigma, power_primary, power_secondary, hardw_ip, sic_ip", data_parameter_rate_valid)
+def test_rate_primary_user(number_user, x_u, y_u, x_r, y_r, uav_height_mean, path_loss, s, sigma, power_primary, power_secondary, hardw_ip, sic_ip):
     for mc in range(monte_carlo_samples):
-        s, sigma = fading_rician(rician_factor, power_los)
         channel_gain_primary, channel_gain_secondary = generate_channel(
             s,
             sigma,
